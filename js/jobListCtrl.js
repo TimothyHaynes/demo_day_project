ed102746d1ba02fe0577c38ef59ee38032982d98
Timothy Haynes <tim15811@gmail.com>
2016-08-09 11:13:45 -0400

    almost there with map in angular

---
+++ js/jobListCtrl.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp');
+app.controller("jobListCtrl", function($http, $scope) {
+    $http({
+        method: 'GET',
+        url: 'http://service.dice.com/api/rest/jobsearch/v1/simple.json?city=48207&direct=1&state=MI'
+    }).then(function successCallback(response) {
+            $scope.items = response.data.resultItemList;
+            console.log($scope.items);
+        },
+        function errorCallback(response) {
+            console.log(response);
+        });
+});

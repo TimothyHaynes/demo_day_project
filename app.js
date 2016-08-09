353a2d4c783141b1f80515c8a67cb92f9bcc0048
Dave Brunetti <brunetti.dave@gmail.com>
2016-08-09 09:51:10 -0400

    dice api functional

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var app = angular.module('myApp', []);
 app.controller("myCtrl1", function($http, $scope) {
     $http({
         method: 'GET',
-        url: 'https://www.reddit.com/r/learnprogramming/.json'
+        url: 'http://service.dice.com/api/rest/jobsearch/v1/simple.json?city=48207&direct=1&state=MI'
     }).then(function successCallback(response) {
-            $scope.items = response.data.data.children;
+            $scope.items = response.data.resultItemList;
             console.log($scope.items);
         },
         function errorCallback(response) {
@@ -18,29 +18,29 @@ app.controller("myCtrl1", function($http, $scope) {
         });
 });
 
-app.controller("myCtrl2", function($http, $scope) {
-    $http({
+// app.controller("myCtrl2", function($http, $scope) {
+//     $http({
 
-        method: 'GET',
-        url: 'https://www.udacity.com/public-api/v0/courses'
-    }).then(function successCallback(response) {
-            $scope.items = response.data.courses;
-            console.log($scope.items);
-        },
-        function errorCallback(response) {
-            console.log(response);
-        });
-});
+//         method: 'GET',
+//         url: 'https://www.udacity.com/public-api/v0/courses'
+//     }).then(function successCallback(response) {
+//             $scope.items = response.data.courses;
+//             console.log($scope.items);
+//         },
+//         function errorCallback(response) {
+//             console.log(response);
+//         });
+// });
 
-app.controller("myCtrl3", function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: 'https://medium.com/free-stuff/?format=json'
-    }).then(function successCallback(response) {
-            $scope.items = response.data;
-            console.log($scope.items);
-        },
-        function errorCallback(response) {
-            console.log(response);
-        });
-});
+// app.controller("myCtrl3", function($http, $scope) {
+//     $http({
+//         method: 'GET',
+//         url: 'https://medium.com/free-stuff/?format=json'
+//     }).then(function successCallback(response) {
+//             $scope.items = response.data;
+//             console.log($scope.items);
+//         },
+//         function errorCallback(response) {
+//             console.log(response);
+//         });
+// });

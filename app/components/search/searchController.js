c171e81a0132f9b059134b25f551ab5ac5ccb8f8
JBeckius <jbeckius91@gmail.com>
2016-08-10 13:22:02 -0400

    commented out post call to node. Safe to merge with master.

--- app/components/search/searchController.js
+++ app/components/search/searchController.js
@@ -20,5 +20,5 @@ app.controller("searchCtrl", function($http, $scope) {
     //       function errorCallback(response) {
     //           console.log(response);
     //       });
-    // }
+    }
 });

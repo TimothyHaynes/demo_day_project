ae917398ecf61a95e3f9b3ddf20b7339d33bb1ba
JBeckius <jbeckius91@gmail.com>
2016-08-10 13:20:31 -0400

    slightly altered searchController passed object. Works now.

--- app/components/search/searchController.js
+++ app/components/search/searchController.js
@@ -1,24 +1,24 @@
 var app = angular.module('myApp');
 //this controller will be requesting data from the API
 app.controller("searchCtrl", function($http, $scope) {
-    $scope.search = function() {
-
-      var zipcode = {'city': "" + $scope.zipcode};
-      console.log(zipcode);
-      // var string = "" + zipcode;
-      $http.post('http://localhost:8080/api/jobs', zipcode)
-      // ({
-      //     method: 'POST',
-      //     url: 'http://localhost:8080/api/jobs',
-      //     data: {'zipcode': string}
-      // })
-      .then(function successCallback(response) {
-              $scope.items = response.data
-              // $scope.job = response.data.#;
-              console.log($scope.items);
-          },
-          function errorCallback(response) {
-              console.log(response);
-          });
-    }
+    // $scope.search = function() {
+    //
+    //   var zipcode = {'city': "" + $scope.zipcode};
+    //   console.log(zipcode);
+    //   // var string = "" + zipcode;
+    //   $http.post('http://localhost:8080/api/jobs', zipcode)
+    //   // ({
+    //   //     method: 'POST',
+    //   //     url: 'http://localhost:8080/api/jobs',
+    //   //     data: {'zipcode': string}
+    //   // })
+    //   .then(function successCallback(response) {
+    //           $scope.items = response.data
+    //           // $scope.job = response.data.#;
+    //           console.log($scope.items);
+    //       },
+    //       function errorCallback(response) {
+    //           console.log(response);
+    //       });
+    // }
 });

cd2217f589b12fa880f397553c27f3a4da2f86d7
Dave Brunetti <brunetti.dave@gmail.com>
2016-08-10 09:01:27 -0400

    persistant map

--- app/components/search/searchController.js
+++ app/components/search/searchController.js
@@ -1,14 +1,16 @@
-var app = angular.module('projectApp');
+var app = angular.module('myApp');
 //this controller will be requesting data from the API
 app.controller("searchCtrl", function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: ''
-    }).then(function successCallback(response) {
-            $scope.job = response.data.#;
-            console.log($scope.items);
-        },
-        function errorCallback(response) {
-            console.log(response);
-        });
+    // $http({
+    //     method: 'GET',
+    //     url: ''
+    // }).then(function successCallback(response) {
+    //         $scope.job = response.data.#;
+    //         console.log($scope.items);
+    //     },
+    //     function errorCallback(response) {
+    //         console.log(response);
+    //     });
 });
+
+//does this actually need to do anything? Maybe send info to the factory?

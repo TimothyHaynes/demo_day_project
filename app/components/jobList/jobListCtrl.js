62506ccc846d1fa0697ae7e863e421028cfa9949
Dave Brunetti <brunetti.dave@gmail.com>
2016-08-10 13:21:03 -0400

    Fixed JobListCtrl initMap function

--- app/components/jobList/jobListCtrl.js
+++ app/components/jobList/jobListCtrl.js
@@ -1,17 +1,40 @@
 var app = angular.module('myApp');
 app.controller("jobListCtrl", function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: 'http://service.dice.com/api/rest/jobsearch/v1/simple.json?city=48207&direct=1&state=MI'
-    }).then(function successCallback(response) {
-            $scope.items = response.data.resultItemList;
-            console.log($scope.items);
-        },
-        function errorCallback(response) {
-            console.log(response);
-        });
+   $scope.searchObject = inputFactory.returnObject();
+   console.log($scope.searchObject);
+   $http({
+       method: 'GET',
+       url: 'http://service.dice.com/api/rest/jobsearch/v1/simple.json?city=48207&direct=1&state=MI'
+   }).then(function successCallback(response) {
+           $scope.items = response.data.resultItemList;
+       },
+       function errorCallback(response) {
+           console.log(response);
+       });
+   $scope.globalMap;
+
+   function initMap(){
+   $scope.globalMap = new google.maps.Map(document.getElementById('job-map'), {
+       center: {
+           lat: 42.2814,
+           lng: -83.7483
+       },
+       scrollwheel: true,
+       zoom: 10
+       });
+   }
+   initMap();
+   setTimeout(function () {
+   // google.maps.event.trigger($scope.model.globalMap, 'resize');
+   // $scope.model.globalMap.setCenter({
+   //         lat: 42.2814,
+   //         lng: -83.7483
+   //     });
+   initMap();
+}, 100);
+
 });
 
 // app.controller('searchOutputCtrl', function($scope, inputFactory) {
-//
+
 // });
